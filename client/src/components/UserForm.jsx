e4fbb86283c471aff02634772b0fae162d96c163
hannahbanana11 <cunananhannahcamille@gmail.com>
2025-07-31 10:59:03 +0800

    first commit

---
+++ client/src/components/UserForm.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import axios from 'axios';
+const UserForm = () => {
+    const [user, setUser] = useState({name:'', email:''});
+    const handleChange = e => setUser({...user, [e.target.name] :e.target.value})
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const result = await axios.post('http://localhost:5000/3d_crud/users',user);
+        console.log(result);
+        setUser({name:'', email:''})
+    }
+    return(
+        <form onSubmit={handleSubmit}>
+            <input name="name" value={user.name} onChange={handleChange} placeholder="PLEASE ENTER YOUR NAME" />
+            <input name="email" value={user.email} onChange={handleChange} placeholder="PLEASE ENTER YOUR EMAIL" />
+            <button type="submit"> ADD USER </button>
+        </form>
+    )
+}
+export default UserForm;
---
+++ server/routes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./db');
+
+router.post('/users', async (req, res) => {
+    const {name, email} = req.body;
+    try{
+        const [result] = await db.execute('INSERT INTO users (name, email) VALUES (?, ?)', [name, email]);
+        res.send({message: 'Success'});
+    }catch (err){
+        res.status(500).send(err);
+    }
+})
+
+router.get('/users', async (req, res) => {
+    try{
+        const [rows] = await db.execute('SELECT * FROM users');
+        res.send(rows);
+    }catch(err){
+        res.status(500).send(err);
+    }
+})
+
+router.put('/users/:id', async (req, res) => {
+    const {name, email} = req.body;
+    try {
+        db.execute('UPDATE users SET name = ?, email= ? WHERE id = ?', [name, email, req.params.id])
+        res.send({id: req.params.id. name, email})
+    }catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+router.delete('/users/:id', async (req, res) => {
+    try{
+        await db.execute('DELETE FROM users WHERE id = ?', [req.params.id]);
+        res.send({message:'User Deleted'})
+    }catch(err){
+        res.status(500).send(err)
+    }
+})
+
+module.exports = router;
