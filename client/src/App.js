5e8cad1dc71a4c28c7f29346039611fbf226987c
hannahbanana11 <cunananhannahcamille@gmail.com>
2025-08-07 08:41:36 +0800

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import UserForm from './components/UserForm';
+import UserForm  from './components/UserForm';
 import UserList from './components/UserList';
-
 function App() {
   return (
     <div className="App">
-      <UserForm/>
-      <UserList/>
+      <UserForm />    
+      <UserList />
     </div>
   );
 }
